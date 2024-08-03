619e4e4a7109d1f36743c22f29ff1b48b58d13b7
Yasin Çoban <cobanyasin1998@gmail.com>
2024-08-03 23:40:16 +0300

    init

---
+++ src/corePackages/Core.Packages/Core.Persistence/Repositories/Entity.cs
@@ -0,0 +1,18 @@
+﻿namespace Core.Persistence.Repositories;
+
+public class Entity<TId>
+{
+    public Entity()
+    {
+        Id = default!;
+    }
+    public Entity(TId id)
+    {
+        Id = id;
+    }
+    public TId Id { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime? UpdatedDate { get; set; }
+    public DateTime? DeletedDaate { get; set; }
+
+}
---
+++ src/rentACar/RentACar/Domain/Entities/Brand.cs
@@ -0,0 +1,17 @@
+﻿using Core.Persistence.Repositories;
+
+namespace Domain.Entities;
+
+public class Brand : Entity<Guid>
+{
+    public Brand()
+    {
+
+    }
+    public Brand(Guid id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+    public string Name { get; set; } = null!;
+}
